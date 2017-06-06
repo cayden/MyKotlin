33c9c69ca756f75758ace32dd822c60f8ebf3fdc
caydencui <caydencui@tencent.com>
2017-06-06 16:31:25 +0800

    @log 添加跳转

--- app/src/main/kotlin/MainActivity.kt
+++ app/src/main/kotlin/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.mykotlin
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
@@ -11,6 +12,13 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         tv.text = "Hello MyKotlin"
         tv.textSize = 20.0f
+
+        btn_test.setOnClickListener {
+            startActivity(Intent(MainActivity@this, TestActivity::class.java))
+        }
+        
     }
+
+
 }
 
---
+++ app/src/main/kotlin/TestActivity.kt
@@ -0,0 +1,12 @@
+package com.example.mykotlin
+import android.app.Activity
+import android.os.Bundle
+
+
+class TestActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test)
+    }
+}
